fcc139faff255735587c077ac1bc40d2d097d0af
monicaavantika-netizen <monicaavantika@gmail.com>
2025-09-18 10:33:49 +0700

    Add files via upload

---
+++ Tugas.py
@@ -0,0 +1,25 @@
+#tugas ke-1
+#iidentitas
+nama = "monica putri"
+umur = 18
+tinggi_badan = 154
+angka_favorit = 30
+
+print("nama:",nama)
+print("umur:",umur)
+print("tinggi_badan:",tinggi_badan)
+print("angka_faavorit:",angka_favorit)
+
+#tugas ke-2
+#harga belanja
+harga_pensil = 2000
+jumlah_pensil = 4
+harga_buku = 5000
+jumlah_buku = 2
+print(harga_pensil* jumlah_pensil + harga_buku*jumlah_buku)
+
+#tugas ke-3
+if angka_favorit % 30 == 0 :
+    print("angka genap")
+else:
+    print("angka ganjil")
